aee0c0f979a2c852949e016a545dbf0d832b44be
Jan Carlo Abalos <73739927+Ariathros@users.noreply.github.com>
2023-03-24 18:23:44 +0800

    edited embeds

--- user/dashboard/index.php
+++ user/dashboard/index.php
@@ -16,10 +16,14 @@
     <h1>Dashboard</h1>
     <p>Here is your dashboard.</p>
 
-    <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/2079418/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&timescale=60&title=Temperature&type=line"></iframe>
-    <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/2079418/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=pH+Level&type=line"></iframe><br>
-    <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/2079418/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Dissolved+Oxygen&type=line"></iframe>
-    <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/2079418/charts/4?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Total+Dissolved+Solids&type=line"></iframe>
+    <div class="p-3 border">
+        <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/2079467/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Temperature&type=line"></iframe>
+        <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/2079467/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=pH+Level&type=line"></iframe>
+        <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/2079467/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Dissolved+Oxygen&type=line"></iframe>
+        <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/2079467/charts/4?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Total+Dissolved+Solids&type=line"></iframe>
+    </div>
+
+
 </body>
 
 </html>
