ca9247a924120f855e806221c9e1f638b1c0d398
theamalimban <72680199+theamalimban@users.noreply.github.com>
2023-07-07 05:23:24 +0800

    Update calculations.php

--- user/options/calculations.php
+++ user/options/calculations.php
@@ -16,68 +16,62 @@
         include "../includes/sidebar.php";
     ?>
     <div class="calculations">
-        <h1>Calculations</h1>
-        <HR></HR>
-
-        <form method="post" action="">
-            <div class="input-group mb-3">
-                <span class="input-group-text">Mean fish seed weight</span>
-                <input type="number" class="form-control" name="FWS">
-                <span class="input-group-text end">kg</span>
-            </div>
-            <div class="input-group mb-3">
-                <span class="input-group-text">Holding density at harvest</span>
-                <input type="number" class="form-control" name="HD">
-                <span class="input-group-text end">kg/ha</span>
-            </div>
-            <div class="input-group mb-3">
-                <span class="input-group-text">Pond surface</span>
-                <input type="number" class="form-control" name="PV">
-                <span class="input-group-text end">ha</span>
-            </div>
-            <div class="input-group mb-3">
-                <span class="input-group-text">Survival rate of fish seed</span>
-                <input type="number" class="form-control" name="SR">
-                <span class="input-group-text end">%</span>
-            </div>
-            <div class="input-group mb-3">
-                <span class="input-group-text">Mean fish weight at harvest</span>
-                <input type="number" class="form-control" name="FW">
-                <span class="input-group-text end">kg</span>
-            </div>
-            <div class="input-group mb-3">
-                <span class="input-group-text">Food conversion ratio</span>
-                <input type="number" class="form-control" name="FCR">
-                <span class="input-group-text end"></span>
-            </div>
-            <div class="input-group mb-3">
-                <span class="input-group-text">Seed cost</span>
-                <input type="number" class="form-control" name="SC">
-                <span class="input-group-text end">₱/kg</span>
-            </div>
-            <div class="input-group mb-3">
-                <span class="input-group-text">Feed cost</span>
-                <input type="number" class="form-control" name="FC">
-                <span class="input-group-text end">₱/kg</span>
-            </div>
-            <div class="input-group mb-3">
-                <span class="input-group-text">Other cost</span>
-                <input type="number" class="form-control" name="AC">
-                <span class="input-group-text end">₱</span>
-            </div>
-            <div class="input-group mb-3">
-                <span class="input-group-text">Interest rate for borrowed funds</span>
-                <input type="number" class="form-control" name="IRC">
-                <span class="input-group-text end">%</span>
-            </div>
-            <div class="input-group mb-3">
-                <span class="input-group-text">Fish price at harvest</span>
-                <input type="number" class="form-control" name="FPH">
-                <span class="input-group-text end">₱/kg</span>
-            </div>
-            <div class="row justify-content-center mb-3">
-                <div class="col-auto">
-                    <input class="btn btn-primary fs-5" type="submit" name="Result" value="Show Results">
+        <h1>Return of Investment Calculator</h1>
+        <!--<p>Calculate here mwehehe.</p>--->
+        <div class="container">
+            <div class="row align-items-start">
+                <div class="col-6">
+                    <form  method="post" action="">
+                        <div class="input-group mb-3 has-validation">
+                            <span class="input-group-text">Start of Farming</span>
+                            <input type="date" class="form-control" name="start_date" required>
+                        </div>
+                        <div class="input-group mb-3 has-validation">
+                            <span class="input-group-text">Total expenses</span>
+                            <input type="number" class="form-control" name="expenses" required>
+                            <span class="input-group-text end">₱</span>
+                        </div>
+                        <div class="input-group mb-3 has-validation">
+                            <span class="input-group-text">Harvest Time</span>
+                            <input type="date" class="form-control" name="harvest_date" required>
+                        </div>
+                        <div class="input-group mb-3 has-validation">
+                            <span class="input-group-text">Total harvest</span>
+                            <input type="number" class="form-control" name="harvest" required>
+                            <span class="input-group-text end">kg</span>
+                        </div>
+                        <div class="input-group mb-3 has-validation">
+                        <span class="input-group-text">Market price</span>
+                            <input type="number" class="form-control" name="price" required>
+                            <span class="input-group-text end">₱/kg</span>
+                        </div>
+                        <div class="row justify-content-center mb-3">
+                            <div class="col-auto">
+                                <input class="btn btn-primary fs-5" type="submit" name="calcu" value="Calculate ROI">
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-6">
+                    <div class="input-group mb-3">
+                        <span class="input-group-text">Sales</span>
+                        <input type="number" class="form-control" name="sales" value="<?php echo $sales; ?>">
+                        <span class="input-group-text end">₱</span>
+                    </div>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text">Profit</span>
+                        <input type="number" class="form-control" name="profit" value="<?php echo $profit; ?>">
+                        <span class="input-group-text end">₱</span>
+                    </div>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text">ROI</span>
+                        <input type="number" class="form-control" name="roi" value="<?php echo $roi; ?>">
+                        <span class="input-group-text end">%</span>
+                    </div>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text">Duration</span>
+                        <input type="" class="form-control" name="roi" value="<?php echo $interval->format("%m months and %d days"); ?>">
+                    </div>
                 </div>
             </div>
         </div>
@@ -104,69 +98,6 @@
                 $interval = date_diff($start_date,$harvest_date);
             }
         ?>
-
-        <div>
-            <div class="input-group mb-3">
-                <span class="input-group-text">Holding density at harvest</span>
-                <input type="number" class="form-control" name="HDN" value="<?php echo $HDN; ?>" DISABLED>
-                <span class="input-group-text end">kg/ha</span>
-            </div>
-            <div class="input-group mb-3">
-                <span class="input-group-text">Total weight of fish</span>
-                <input type="number" class="form-control" name="WH" value="<?php echo $WH; ?>" DISABLED>
-                <span class="input-group-text end">kg</span>
-            </div>
-            <div class="input-group mb-3">
-                <span class="input-group-text">Total fish biomass</span>
-                <input type="number" class="form-control" name="BH" value="<?php echo $BH; ?>" DISABLED>
-                <span class="input-group-text end"></span>
-            </div>
-            <div class="input-group mb-3">
-                <span class="input-group-text">Number of seed</span>
-                <input type="number" class="form-control" name="NS" value="<?php echo $NS; ?>" DISABLED>
-                <span class="input-group-text end"></span>
-            </div>
-            <div class="input-group mb-3">
-                <span class="input-group-text">Feed needed to produce biomass at harvest</span>
-                <input type="number" class="form-control" name="FN" value="<?php echo $FN; ?>" DISABLED>
-                <span class="input-group-text end">kg</span>
-            </div>
-            <div class="input-group mb-3">
-                <span class="input-group-text">Total cost for seed</span>
-                <input type="number" class="form-control" name="TSC" value="<?php echo $TSC; ?>" DISABLED>
-                <span class="input-group-text end">₱</span>
-            </div>
-            <div class="input-group mb-3">
-                <span class="input-group-text">Total cost for feed</span>
-                <input type="number" class="form-control" name="TFC" value="<?php echo $TFC; ?>" DISABLED>
-                <span class="input-group-text end">₱</span>
-            </div>
-            <div class="input-group mb-3">
-                <span class="input-group-text">Total cost</span>
-                <input type="number" class="form-control" name="TC" value="<?php echo $TC; ?>" DISABLED>
-                <span class="input-group-text end">₱</span>
-            </div>
-            <div class="input-group mb-3">
-                <span class="input-group-text">Break-even price</span>
-                <input type="number" class="form-control" name="BEP" value="<?php echo $BEP; ?>" DISABLED>
-                <span class="input-group-text end">₱/kg</span>
-            </div>
-            <div class="input-group mb-3">
-                <span class="input-group-text">Revenue</span>
-                <input type="number" class="form-control" name="REV" value="<?php echo $REV; ?>" DISABLED>
-                <span class="input-group-text end">₱</span>
-            </div>
-            <div class="input-group mb-3">
-                <span class="input-group-text">Profit</span>
-                <input type="number" class="form-control" name="PRO" value="<?php echo $PRO; ?>" DISABLED>
-                <span class="input-group-text end">₱</span>
-            </div>
-            <div class="input-group mb-3">
-                <span class="input-group-text">Return of Investment</span>
-                <input type="number" class="form-control" name="ROI" value="<?php echo $ROI; ?>" DISABLED>
-                <span class="input-group-text end">%</span>
-            </div>
-        </div>
     </div>
 
     
