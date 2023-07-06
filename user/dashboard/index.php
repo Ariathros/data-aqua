fcf2b9846c9faf93d9b0915127e20f415dc24577
theamalimban <72680199+theamalimban@users.noreply.github.com>
2023-07-07 05:25:13 +0800

    Update index.php

--- user/dashboard/index.php
+++ user/dashboard/index.php
@@ -7,6 +7,8 @@
 <head>
     <title>Dashboard</title>
     <?php include("../../includes/bootstrap-header.php"); ?>
+    <link rel="stylesheet" href="../../assets/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <?php
